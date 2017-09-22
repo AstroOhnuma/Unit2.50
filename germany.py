05fe9272d50c0d418aedbcf41f99af4816a062f7
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-22 14:54:24 -0400

    Updated from Brython Server: 9/22/2017 2:54:24 PM

--- germany.py
+++ germany.py
@@ -1,1 +1,12 @@
+#Astro Ohnuma
+#9/22/17
+#germany.py - Making the german flag with ggame graphics
+
+from ggame import *
+
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+orange = Color(0xFF8000,1)
+
+blackoutline = LineStyle(1,black)
 
