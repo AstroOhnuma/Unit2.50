da6525bb5dc2a2286d921a3ceb3688369dcf1f21
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-26 14:50:27 -0400

    Updated from Brython Server: 9/26/2017 2:50:27 PM

--- name.py
+++ name.py
@@ -1,1 +1,19 @@
+#Astro Ohnuma
+#9/26/17
+#name.py - asking for a name and a color code and then putting that name on a background of that color
 
+from ggame import *
+name = input('Enter your name: ')
+colorcode = input('Enter an RGB color code: ')
+
+color = Color(colorcode,1)
+black = Color(0x000000,1)
+
+blackoutline = LineStyle(1,black)
+
+background = RectangleAsset(1400,600,blackoutline,color)
+text = TextAsset(name,fill=black,style='bold 60pt Times')
+
+Sprite(background, (0,0))
+Sprite(text, (575,200))
+App().run()
