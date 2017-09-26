e114de9699cfa1ea5d06cd7ed6c825f1c3fcbc82
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-26 14:54:32 -0400

    Updated from Brython Server: 9/26/2017 2:54:32 PM

--- yield.py
+++ yield.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#9/26/17
+#yield.py - creating a yield sign
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+white = Color(0xFFFFFF,1)
+
+redoutline = LineStyle(1,red)
+whiteoutline = LineStyle(1,white)
