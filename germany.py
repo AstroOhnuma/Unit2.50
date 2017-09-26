204cc5930b3f7ad6f3a84d27fd6be97a4c79e270
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-26 14:34:41 -0400

    Updated from Brython Server: 9/26/2017 2:34:41 PM

--- germany.py
+++ germany.py
@@ -6,7 +6,17 @@ from ggame import *
 
 black = Color(0x000000,1)
 yellow = Color(0xFFFF00,1)
-orange = Color(0xFF8000,1)
+red = Color(0xFF0000,1)
 
 blackoutline = LineStyle(1,black)
+yellowoutline = LineStyle(1,yellow)
+redoutline = LineStyle(1,red)
 
+blackrectangle = RectangleAsset(300,50,blackoutline,black)
+redrectangle = RectangleAsset(300,50,redoutline,red)
+yellowrectangle = RectangleAsset(300,50,yellowoutline,yellow)
+
+Sprite(blackrectangle,(100,100))
+Sprite(redrectangle, (100,150))
+Sprite(yellowrectangle,(100,200))
+App().run()
